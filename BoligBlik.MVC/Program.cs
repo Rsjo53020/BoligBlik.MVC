2315e011f0d9fee581e28a3fb82a0e382d8e7ac6
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2024-05-29 14:03:25 +0200

    docker compose samlet igen

--- BoligBlik.MVC/Program.cs
+++ BoligBlik.MVC/Program.cs
@@ -16,8 +16,8 @@ namespace BoligBlik.MVC
                 // Add services to the container -- move to a ServiceExtension.cs
 
                 //.GetConnectionString("RSFrontEndConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-                .GetConnectionString("AlexFrontEndLocalConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            //.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                //.GetConnectionString("AlexFrontEndLocalConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            .GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 
             // move to a ServiceExtension.cs
@@ -83,7 +83,6 @@ namespace BoligBlik.MVC
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
